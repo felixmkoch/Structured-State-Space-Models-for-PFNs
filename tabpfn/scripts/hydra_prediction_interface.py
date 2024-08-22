2316e061af3b3310e96113cbbd56c571b1cf8a0f
Felix Koch <70198454+felixmkoch@users.noreply.github.com>
2024-08-22 23:15:07 +0200

    Fix hydra mistaken for mamba bug

--- tabpfn/scripts/hydra_prediction_interface.py
+++ tabpfn/scripts/hydra_prediction_interface.py
@@ -104,7 +104,7 @@ def load_model_workflow(i, e, add_name, base_path, device='cpu', eval_addition='
     print(f'Loading {model_file}')
     if only_inference:
         # print('Loading model that can be used for inference only')
-        model, c = load_model_only_inference(base_path, model_file, device, model_name="mamba")
+        model, c = load_model_only_inference(base_path, model_file, device, model_name="hydra")
     else:
         #until now also only capable of inference
         model, c = load_model(base_path, model_file, device, eval_positions=[], verbose=False)
